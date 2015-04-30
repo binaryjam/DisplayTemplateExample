20aa42969c22c498a7a91a0a6749ffa65ff6a102
ftpsimon <simon.tocker@eon.com>
2015-04-30 16:49:25 +0100

    comments

--- test1.js
+++ test1.js
@@ -1,4 +1,5 @@
-/* global nsDTColourTestList */
+/* global EnsureScriptFunc */    //No Types for init.js, shame really.  Have to put this here as Type.registerNamespace doesnt return anything for intellisense to pick up on
+/* global nsDTColourTestList */  //Intellisense doesnt work for revealing module pattern, rather annoying
 /// <reference path="typings/sharepoint/sharepoint.d.ts"/>
 /// <reference path="typings/microsoft-ajax/microsoft.ajax.d.ts"/>
 
@@ -8,6 +9,7 @@
 //Reference Blogs
 //	 Martin Hatch - http://www.martinhatch.com/2013/08/jslink-and-display-templates-part-1.html
 //   Wictor Wilen - http://www.wictorwilen.se/the-correct-way-to-execute-javascript-functions-in-sharepoint-2013-mds-enabled-sites
+//   Hugh Wood - http://www.spcaf.com/blog/sharepoint-javascript-context-development-part-4-the-way-of-the-async-delta-manager/
 //So what is this ?
 //   I'm trying to create an Display Template that uses the best practices I can pull together and hopefully
 //	 by using JS Patterns produce something that works efficiently and elegantly
@@ -17,14 +19,15 @@
 //   Early days so don't expect elegant or efficient yet :-)
 //
 
+//Creates the namespace and registers it so MDS knows its there, 
 Type.registerNamespace('nsDTColourTestList');
 
 (function(ns) {
 	//private members
 	var overrides = {};
   	overrides.Templates = {};
-  	overrides.Templates.OnPostRender = onPostRender;   //As this is associated with a template not sure why its firing lots of times.
-
+  	overrides.Templates.OnPostRender = onPostRender;   //As this is associated with a template not sure why its firing lots of times on DisplayForm.aspx.
+														// but not for AllItems.aspx
    	overrides.Templates.Fields = {
        //Colour is the Name of our field
        'Colour': {
@@ -56,7 +59,7 @@ Type.registerNamespace('nsDTColourTestList');
 		//Due to lifecycle, you cannot Ensure mquery load till later, so Im doing it here
 		//because doing it earlier didnt work.
 		
-		//Force sync loading to prevent race conditions
+		//Force sync loading to prevent race conditions, I suppose it depends what you do here.
 		EnsureScriptFunc('mQuery.js', 'm$', function() {
 		    console.log("mquery callback");
 		}, false);
@@ -80,7 +83,10 @@ Type.registerNamespace('nsDTColourTestList');
 
 })(nsDTColourTestList);
 
-
+//I thought this odd, but it must be that, if you are in the context of MDS then this object 
+//exists, if it doesnt exist then your are in a normal page, but this object exists in normal pages too
+//yes but not at this point in the lifecycle.
+//What a really strange way of determining in MDS mode or not.
 if (typeof _spPageContextInfo != "undefined" && _spPageContextInfo != null) {
 	console.log("Starting Display Override MDS");
 	nsDTColourTestList.MdsRegisterTemplateOverrides();
